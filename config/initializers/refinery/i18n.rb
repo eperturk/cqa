2db13ca5f951168d82edfb2819f1dcc4371f4740
Erol Erturk <erolerturk@Erol-Erturks-MacBook-Pro.local>
2013-05-26 11:43:10 -0400

    twitter bootstrap and refinery

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  # config.enabled = true
+
+  # config.default_locale = :en
+
+  # config.current_locale = :en
+
+  # config.default_frontend_locale = :en
+
+  # config.frontend_locales = [:en]
+
+  # config.locales = {:bg=>"Български", :es=>"Español", :"zh-TW"=>"Traditional Chinese", :pl=>"Polski", :"pt-BR"=>"Português", :fr=>"Français", :it=>"Italiano", :nl=>"Nederlands", :en=>"English", :el=>"Ελληνικά", :de=>"Deutsch", :rs=>"Srpski", :lv=>"Latviski", :sk=>"Slovenský", :cs=>"Česky", :nb=>"Norsk Bokmål", :da=>"Dansk", :ru=>"Русский", :ja=>"日本語", :sl=>"Slovenian", :"zh-CN"=>"Simplified Chinese", :sv=>"Svenska"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cqa::Application.config.secret_token = 'd1d2eda7f71a93c92763c9cd062449244771613ad7705ce5554df57aa25ed9eb6e13194c65ca065e6da8ee5fa5e73e73a12b1d3028cd1e3aaa04bd89c27122d1'
